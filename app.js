80b36d141411f1f0f39c154ccb02f573e5708dcc
Christopher Gregorian <csgregorian@gmail.com>
2014-07-26 19:41:49 -0400

    Added DB modules, minor updates

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express'); // Web framework
 var app = express();
 var http = require('http').Server(app);
+var cookieParser = require('cookie-parser'); // Better cookies
 var io = require('socket.io')(http); // Real-time web sockets
 var stylus = require('stylus'); // Better CSS
 var nib = require('nib'); // Adds vendor-prefix support to stylus
 var jade = require('jade'); // Smart HTML templating
+var morgan = require('morgan'); // Connection logging
+var mongo = require('mongodb'); // NoSQL database
+var monk = require('monk'); // Better Mongo interactions
+
+var db = monk('localhost:27017/boilerplate'); // Listens on database server
 
 function compile(str, path) {
     return stylus(str)
@@ -23,19 +29,21 @@ app.use(stylus.middleware(
      }
 ))
 app.use(express.static(__dirname + '/public'))
+app.use(cookieParser());
+app.use(morgan('common'));
 
 // Routing
 app.get('/', function(req, res){
   res.render('index')
 });
 
-// Connection logging
-io.sockets.on('connection', function(socket){
-  console.log('A user has connected!');
-  socket.on('disconnect', function(){
-    console.log('A user has disconnected!');
-  });
-});
+// Sockets
+// io.sockets.on('connection', function(socket){
+//   console.log('A user has connected!');
+//   socket.on('disconnect', function(){
+//     console.log('A user has disconnected!');
+//   });
+// });
 
 // Server
 http.listen(3000, function(){
